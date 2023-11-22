d3a49eb96185d36b6ebaad255dd7b02368ddb442
Haiyan Wang <haiyan.wang916@duke.edu>
2023-11-21 20:12:21 -0500

    added grid init

--- src/T4.py
+++ src/T4.py
@@ -11,11 +11,11 @@ import datetime as dt
 import random
 import time
 import multiprocessing
-
-
+import math
 
 ### Data Objects
 NODES = {} # <node_id: Node_Object>
+NODE_COORDS = {} # <(lat, lon): Node_Object>
 DRIVERS = []
 PASSENGERS = []
 
@@ -23,23 +23,45 @@ PASSENGERS = []
 AVG_MPH = 0
 NUM_ROADS = 0
 
+### Grid Params
+PARTITIONS = 900
+MINLAT, MINLON, MAXLAT, MAXLON = float('inf'), float('inf'), float('-inf'), float('-inf') # Getting edges for grid partitioning
+GRID = [[[] for i in range(math.ceil(math.sqrt(PARTITIONS)))] for j in range(math.ceil(math.sqrt(PARTITIONS)))]
+GRID_PARAMS = []
 
 def initialize():
 
     rootpath = os.path.dirname(os.getcwd())
-
+    
     ### Initialize nodes
-    with open(rootpath + '/NotUber/data/node_data.json', 'r') as v:
+    with open(rootpath + '/data/node_data.json', 'r') as v:
         n_reader = json.load(v)
 
     # Generate Node objects
     for node_id in n_reader:
         node = classes.Node(id = node_id, lat = n_reader[node_id]['lat'], lon = n_reader[node_id]['lon'])
         NODES[int(node_id)] = node
-    print('Nodes initialized')
+        NODE_COORDS[(n_reader[node_id]['lat'], n_reader[node_id]['lon'])] = node
+
+        # Get edges of grid
+        global MINLAT, MINLON, MAXLAT, MAXLON
+        if n_reader[node_id]['lat'] < MINLAT:
+            MINLAT = n_reader[node_id]['lat']
+        if n_reader[node_id]['lon'] < MINLON:
+            MINLON = n_reader[node_id]['lon']
+        if n_reader[node_id]['lat'] > MAXLAT:
+            MAXLAT = n_reader[node_id]['lat']
+        if n_reader[node_id]['lon'] > MAXLON:
+            MAXLON = n_reader[node_id]['lon']
     
+    # Set grid params
+    global GRID_PARAMS
+    GRID_PARAMS.extend([PARTITIONS, MINLAT, MAXLAT, MINLON, MAXLON])
+    for node in NODES.values():
+        node.partition(GRID, GRID_PARAMS)
+
     ### Initialize edges
-    with open(rootpath + '/NotUber/data/edges.csv', 'r') as e:
+    with open(rootpath + '/data/edges.csv', 'r') as e:
         _ = e.readline()
         e_reader = csv.reader(e)
 
@@ -66,70 +88,38 @@ def initialize():
             AVG_MPH += avg_speed
             NUM_ROADS += 1
 
-    print('Edges initialized')
-
     ### Initialize drivers
-    with open(rootpath + '/NotUber/data/drivers.csv', 'r') as d:
+    with open(rootpath + '/data/drivers.csv', 'r') as d:
         _ = d.readline()
         d_reader = csv.reader(d)
         
         # Generate Driver objects
         id = 1 # IDs because the data doesn't come with them
         for d in d_reader:
-            timestamp, lat, lon = d
-            driver = classes.Driver(id = id, timestamp = timestamp, lat = float(lat), lon = float(lon))
+            time, lat, lon = d
+            driver = classes.Driver(id = id, timestamp = time, lat = float(lat), lon = float(lon))
+            driver.node = driver.assign_node(driver.coords, GRID, GRID_PARAMS) # Assign driver to nearest node
             DRIVERS.append(driver)
             id += 1
 
-    # Assign drivers to nearest nodes
-    for driver in DRIVERS:
-        min_dist = float('inf')
-        nearest_node = None
-        for node in NODES.values():
-            dist = driver.euclidean_dist(node)
-            if dist < min_dist:
-                min_dist = dist
-                nearest_node = node
-        driver.node = nearest_node
-    print('Drivers initialized')
-
     ### Initialize passengers
-    with open(rootpath + '/NotUber/data/passengers.csv', 'r') as p:
+    with open(rootpath + '/data/passengers.csv', 'r') as p:
         _ = p.readline()
         p_reader = csv.reader(p)
 
         # Generate Passenger objects
         id = 1 # IDs because the data doesn't come with them
         for p in p_reader:
-            timestamp, start_lat, start_lon, end_lat, end_lon = p
-            passenger = classes.Passenger(id = id, timestamp = timestamp, start_lat = float(start_lat), start_lon = float(start_lon), end_lat = float(end_lat), end_lon = float(end_lon))
+            time, start_lat, start_lon, end_lat, end_lon = p
+            passenger = classes.Passenger(id = id, timestamp = time, start_lat = float(start_lat), start_lon = float(start_lon), end_lat = float(end_lat), end_lon = float(end_lon))
+            passenger.node = passenger.assign_node(passenger.coords, GRID, GRID_PARAMS)
+            passenger.end_node = passenger.assign_node(passenger.end_coords, GRID, GRID_PARAMS)
             PASSENGERS.append(passenger)
             id += 1
-
-    # Assign passengers to nearest nodes
-    for passenger in PASSENGERS:
-        min_dist_start = float('inf')
-        nearest_node_start = None
-        min_dist_end = float('inf')
-        nearest_node_end = None
-        for node in NODES.values():
-            start_dist = passenger.euclidean_dist(node)
-            if start_dist < min_dist_start:
-                min_dist_start = start_dist
-                nearest_node_start = node
-            end_dist = passenger.euclidean_dist(node, time = 'end')
-            if end_dist < min_dist_end:
-                min_dist_end = end_dist
-                nearest_node_end = node
-        passenger.node = nearest_node_start
-        passenger.end_node = nearest_node_end
-    print('Passengers initialized')
 
     ### Average MPH on network
     AVG_MPH /= NUM_ROADS
     print(f'Average MPH: {AVG_MPH}')
-
-    end = time.time()
 
 def main():
     init_start = time.time()
